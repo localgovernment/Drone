5081545e7f85c4d172fe6a13422caf531c1a05d6
Dion Liddell <dionliddell@gmail.com>
2019-01-21 17:31:38 +1300

    Testing Javascript Promises

--- taupodrone.js
+++ taupodrone.js
@@ -1,15 +1,14 @@
+// https://developers.arcgis.com/javascript/latest/sample-code/intro-popup/index.html
 require([
   "esri/Map",
   "esri/views/MapView",
   "esri/layers/FeatureLayer",
-  "esri/widgets/FeatureForm",
-  "esri/widgets/Locate",
-  "esri/widgets/Search",
-  "esri/tasks/Locator",
-  "esri/tasks/support/Query",
-  "dojo/domReady!"
-  ], function(Map, MapView, FeatureLayer, FeatureForm, Locate, Search, Locator, Query){
+  "esri/tasks/Locator"
+], function(
+  Map, MapView, FeatureLayer, Locator
+) {
 
+  // Create the Map
   var map = new Map({
     basemap: "streets-navigation-vector"
   });
@@ -25,7 +24,8 @@ require([
   });
 
   map.add(aerodromes_4k);
-
+ 
+  // Create the MapView
   var view = new MapView({
     container: "viewDiv",  // Reference to the scene div
     map: map,  // Reference to the map object created before the scene
@@ -33,56 +33,65 @@ require([
     center: [175.9, -38.8]  // Sets centre point of view using longitude,latitude
   });   
   
-  var locateBtn = new Locate({
-    view: view
+  // Set up a locator task using the world geocoding service
+  var locatorTask = new Locator({
+    url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+    countryCode:"NZ"
+  });
+
+  /*******************************************************************
+   * This click event sets generic content on the popup not tied to
+   * a layer, graphic, or popupTemplate. The location of the point is
+   * used as input to a reverse geocode method and the resulting
+   * address is printed to the popup content.
+   *******************************************************************/
+  view.popup.autoOpenEnabled = false;
+  view.on("click", function(event) {
+
+    // Get the coordinates of the click on the view
+    var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+    var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+
+    view.popup.open({
+      title: "Aerodrome and Council Information...",
+      location: event.mapPoint // Set the location of the popup to the clicked location
+    });
+
+    /*
+    // view.popup.content = getAddress(event);
+    getAddress(event).then(function(content){
+      // view.popup.content = content;
+      console.log(content);
+    }); */
+    
+    // Display the popup
+    // Execute a reverse geocode using the clicked location
+    locatorTask.locationToAddress(event.mapPoint).then(function(
+      response) {
+      // If an address is successfully found, show it in the popup's content
+      return response.address;
+    }).catch(function(error) {
+      // If the promise fails and no result is found, show a generic message      
+        return "No address was found for this location";
+    });
+    
   });
   
-  var droneRequests = new FeatureLayer({
-    url: "https://services7.arcgis.com/S7DHOirgbYgdtrbR/arcgis/rest/services/Taupo_Drone_Requests/FeatureServer"
-  });
-    
-  // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-FeatureForm.html#feature
-  var droneRequestForm = new FeatureForm({
-    container: "formDiv", // HTML div
-    layer: droneRequests,
-    fieldConfig: [ // Autocasts as FieldConfig
-      { name: "First_Name",
-        label: "First Name"
-      },
-      { name: "Last_Name",
-        label: "Last Name"
-      },
-      { name: "Email",
-        label: "Email"
-      },
-      { name: "Phone",
-        label: "Phone"
-      },
-      {
-        name: "Aerodromes",
-        label: "Aerodromes"
-      }]
-  });
- 
-  // https://gis.stackexchange.com/questions/297918/limiting-the-search-widget-results-to-a-country-in-arcgis-js-api-4-9?rq=1
-  // Set local to NZ
-  var search = new Search({
-    sources: [{
-      locator: new Locator({ url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"}),
-      countryCode:"NZ"
-    }],
-    view: view,
-    includeDefaultSources: false,
-    locationEnabled: false    
-  });
- 
-  // query buffer on click/tap
-  view.on("click", function(event){
-    // https://developers.arcgis.com/javascript/latest/api-reference/esri-tasks-support-Query.html
-    // https://developers.arcgis.com/javascript/latest/sample-code/featurelayer-query/index.html
-    
-    var aerodromes
-    
+  // Execute a reverse geocode using the clicked location
+  function getAddress(event) {
+    content = '<b>Address:</b>';
+    return locatorTask.locationToAddress(event.mapPoint).then(function(response) {
+      console.log(response.address);
+      return content.concat(response.address,'<br>',aerodromes(event));
+    }, function(err) {
+      return content.concat(' No address found at that location');
+    });
+  }
+  
+  /*
+  // return aerodromes at given event as an html formatted list
+  function aerodromes(content, event) {
+  
     var query = aerodromes_4k.createQuery();
     query.geometry = view.toMap(event);  // the point location of the pointer
     query.spatialRelationship = "intersects";  // this is the default
@@ -91,28 +100,20 @@ require([
     
     aerodromes_4k.queryFeatures(query).then(function(result){    
       var aerodromeFeatures = result.features;
-      if (aerodromeFeatures.length) {
-        // Test display
-        droneRequests.queryFeatures(droneRequests.createQuery()).then(function(result){
-          console.log(result);
-          editFeature = result.features[0];
-          droneRequestForm.feature = editFeature;
-          document.getElementById("update").classList.remove("esri-hidden");
-        });
-        
+      if (aerodromeFeatures.length) {   
+        content = '<b>Aerodromes</b><ul>'
         for (var i = 0; i < aerodromeFeatures.length; i++) 
-          console.log(aerodromeFeatures[i].attributes['Aerodrome']);
-        }        
-        else {
-          console.log('Not within 4km of a Taupo Aerodrome');
-        }
-    })    
-  });
-    
-  view.ui.add(search, "top-right");
-  view.ui.add("update", "bottom-right");  
-  view.ui.add(locateBtn, {
-    position: "top-left"
-  });
-    
+          content = content.concat('<li>',aerodromeFeatures[i].attributes['Aerodrome'],'</li>');
+        content = content.concat('</ul>');
+      }        
+      else {
+        content = 'Not within 4km of a Taupo Aerodrome';
+      }
+      view.popup.content = content;
+    })
+
+    return content; 
+  }
+  */
+  
 });
